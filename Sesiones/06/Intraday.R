bab9d5c16dcc772e18ed3db601574a829dbbe8c1
jguerrah <jguerrah@uni.pe>
2025-12-12 21:54:45 -0500

    Updates sesión 6

--- Sesiones/06/Intraday.R
+++ Sesiones/06/Intraday.R
@@ -1,24 +1,68 @@
 
+install.packages("quantmod")
 library(quantmod)
 
-ticker <- "AAPL"
+ticker <- "FRMI" # Fermi Inc.
 period <- "60min" # 30min, 15min, etc.
 
+?getSymbols
 data <- getSymbols(ticker, src = "yahoo", 
                    periodicity = period, auto.assign = FALSE)
 data
 
 ?chartSeries
 chartSeries(data, theme = chartTheme("white"), 
-            name = "AAPL Intraday (60 min)")
+            name = "Intraday (60 min)")
 
 chartSeries(data, subset = "last 5 days",
-            theme = chartTheme("white"), name = "AAPL Intraday (60 min)")
+            theme = chartTheme("white"), name = "Intraday (60 min)")
+
+chartSeries(data, type="line", subset = "last 5 days",
+            name = "Intraday (60 min)")
 
 library(dplyr)
 library(ggplot2)
 
 data <- data.frame(Date = index(data), coredata(data))
-ggplot(data, aes(x = Date, y = AAPL.Adjusted)) +
+data
+
+ggplot(data, aes(x = Date, y = FRMI.Close)) +
   geom_line() +
-  labs(title = "AAPL Adjusted Close (60 min)", x = "Time", y = "Price")
+  labs(title = "FRMI Adjusted Close (60 min)", x = "Time", y = "Price")
+
+
+install.packages("rugarch")
+library(rugarch)
+
+?ugarchspec
+# E[(x-E[x])^2]
+# E[x] = mean.model
+
+returns <- diff(log(data$FRMI.Close))
+
+# Vol: GARCH(1,1) - Mean model: Const
+spec <- ugarchspec(variance.model = list(model="sGARCH", garchOrder=c(1, 1)), 
+                   mean.model = list(armaOrder=c(0,0), include.mean=TRUE), 
+                   distribution.model = "norm")
+
+?ugarchfit
+garch11 <- ugarchfit(spec=spec, data=returns)
+
+class(garch11)
+print(garch11)
+# omega: constante
+# alpha1: coef del error
+# beta1: coef del sigma
+
+plot(garch11)
+
+plot(garch11, which=3)
+
+
+# Vol: EGARCH(1,1) - Mean model: Const
+spec2 <- ugarchspec(variance.model = list(model="eGARCH", garchOrder=c(1, 1)), 
+                    mean.model = list(armaOrder=c(0,0), include.mean=TRUE), 
+                    distribution.model = "std")
+egarch11 <- ugarchfit(spec=spec2, data=returns)
+
+print(egarch11)
--- Sesiones/06/Stochvol.R
+++ Sesiones/06/Stochvol.R
@@ -3,10 +3,53 @@
 # Jose Guerra
 # 2025
 
-install.packages("stochvol")
+rm(list=ls())
+
+### MS GARCH ###
+library(quantmod)
+
+data <- getSymbols('NVDA', src="yahoo", auto.assign=FALSE)
+data
+
+returns <- diff(log(data$NVDA.Adjusted))
+returns <- returns[!is.na(returns)]
+
+install.packages("MSGARCH")
+library(MSGARCH)
+
+?CreateSpec
+spec <- CreateSpec(variance.spec = list(model = c("sGARCH", "sGARCH")), 
+                   distribution.spec = list(distribution = c("norm", "norm")))
+
+?FitML
+garch2 <- FitML(spec=spec, data=returns)
+
+class(garch2)
+# GARCH(1,1) state 1:
+#  - alpha0_1
+#  - alpha1_1
+#  - beta_1
+# GARCH(1,1) state 2:
+#  - alpha0_2
+#  - alpha1_2
+#  - beta_2
+print(garch2)
+
+
+?CreateSpec
+spec <- CreateSpec(variance.spec = list(model = c("sGARCH", "sGARCH")), 
+                   distribution.spec = list(distribution = c("norm", "norm")))
+
+?FitML
+garch2 <- FitML(spec=spec, data=returns)
+
+
+
+### stochvol ###
 
 rm(list=ls())
 
+install.packages("stochvol")
 library("stochvol")
 set.seed(42)
 
@@ -16,12 +59,23 @@ ind <- which(exrates$date >= as.Date("2008-03-01") &
                exrates$date <= as.Date("2012-03-01"))
 CHF_price <- exrates$CHF[ind]
 
+plot(CHF_price, type='l')
+
+?svsample
 res_sv <- svsample(CHF_price, designmatrix = "ar1")
 res_sv
+summary(res_sv)
+plot(res_sv)
+volplot(res_sv)
 
 CHF_logret <- 100 * logret(CHF_price)
 res_svt <- svtsample(CHF_logret, designmatrix = "ar0") # t-errors
 res_svt
+plot(res_svt)
 
 
+# NVDA
+vol_NVDA <- svtsample(returns, designmatrix = "ar0")
+vol_NVDA
+plot(vol_NVDA)
 
