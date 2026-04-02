c3bd06f54bf65c7285db05b18ed50daed9b3057e
jguerrah <jguerrah@uni.pe>
2026-04-01 19:13:43 -0500

    Actualización scrapping con tidyverse

--- Sesiones/07/Webscraping1.R
+++ Sesiones/07/Webscraping1.R
@@ -1,5 +1,8 @@
 # Universidad Nacional de Ingenieria
+# Pre-Maestria
+# R Aplicado
 # Jose Guerra
+# Marzo 2026
 
 rm(list = ls())
 
@@ -27,10 +30,12 @@ rm(list = ls())
 # &includeAdjustedClose=true
 
 
-
+# rvest
+# https://rvest.tidyverse.org/
 install.packages("rvest")
 library(rvest)
 ?rvest
+
 
 # Ejemplo 1
 link <- "https://dataquestio.github.io/web-scraping-pages/simple.html"
@@ -43,7 +48,6 @@ web %>%
 #/html/body/p
 web %>%
   html_elements(xpath="/html/body/p")
-
 
 web %>%
   html_elements("p") %>%
@@ -66,18 +70,25 @@ web %>%
   html_elements("p.temp") %>%
   html_text()
 
+
+# readr
+# https://readr.tidyverse.org/
 library(readr)
+?readr
 
 ?parse_number
-forecast <- web %>%
+web %>%
   html_elements("p.temp") %>%
   html_text() %>%
   parse_number()
-forecast
 
 
+
+library(tidyverse)
+
+# Ejemplo aplicado
 # RPP
-link <- "https://rpp.pe/archivo/2025-03-31"
+link <- "https://rpp.pe/archivo/2026-03-31"
 web <- read_html(link)
 web
 
@@ -85,9 +96,25 @@ titles <- web %>%
   html_elements("h2.news__title") %>%
   html_text()
 titles
+
 titles[2]
 
 
+# stringr
+# https://stringr.tidyverse.org/
+library(stringr)
+?stringr
+
+?str_trim
+titles |>
+  str_trim()
+
+?str_squish
+titles |>
+  str_squish()
+
+
+# Ejemplo aplicado
 # BCRP
 link <- "https://estadisticas.bcrp.gob.pe/estadisticas/series/diarias/resultados/PD04709XD/html/"
 web <- read_html(link)
@@ -98,4 +125,11 @@ embig <- web %>%
   html_table()
 embig <- embig[[1]]
 colnames(embig)[2] <- "EMBIG_Peru"
+embig
+
+embig <- web |>
+  html_elements(xpath='//*[@id="frmDiarias"]/div[3]/table') |>
+  html_table() |>
+  _[[1]] |>
+  rename_with(.cols=2, ~ "EMBIG_Peru")
 embig
